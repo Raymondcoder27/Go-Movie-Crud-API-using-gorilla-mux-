c6758c386bffc7ce9704c7d8542e18f1935173c8
Mwebe Raymond <mwebraymond13@gmail.com>
2024-07-18 18:03:28 +0300

    Router and get Movies methods set

--- movies2.go
+++ movies2.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -42,12 +44,62 @@ func getMovies(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Movies: %v", movies)
 }
 
+func createMovie(w http.ResponseWriter, r *http.Request){
+	//set headers
+	w.Header().Set("Content-Type", "application/json")
+
+	//create movie variable
+	var movie Movie
+
+	//create new movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+
+	movie.ID = strconv.Itoa(rand.Intn(10000))
+	movies = append(movies, movie)
+
+	//return movie
+	json.NewEncoder(w).Encode(movie)
+
+	// fmt.Fprintf(w, "Created Movie: %v", movie)
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request){
+	//set headers
+	w.Header().Set("Content-Type", "application/json")
+
+	//create movie variable
+	var movie Movie
+
+	//get params from vars
+	params := mux.Vars(r)
+
+	//cycle through movies slice and delete specified id
+	for index, item := range movies {
+		if (item.ID == params["id"]){
+			return movies(movies[:index], movies[index+1:]...)
+		}
+	}
+	json.NewEncoder(w).Encode(movie)
+
+}
+
 func getMovie(w http.ResponseWriter, r *http.Request){
 	//set headers 
 	w.Header().Set("Content-Type", "application/json")
 
 	//get the params from vars
-	
+	params := mux.Vars(r)
+
+	//create movie variable
+	var movie Movie
+
+	//cycle through movies slice and compare params id to movie id
+	for index, item := range movies {
+		if (item.ID == params["id"]){
+			return index[movie]
+		}
+	}
+	json.NewEncoder(w).Encode(movie)
 }
 
 
